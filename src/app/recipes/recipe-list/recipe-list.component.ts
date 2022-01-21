0cff6eb812acaefd3ba6aa7e1522d719bbfd8fa0
Tanner Goodale <goo18011@byui.edu>
2022-01-21 15:58:47 -0600

    testing databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', "This is a test", 'http://foodjaunts.com/wp-content/uploads/2012/04/Spring-Shrimp-Salad-Overview-e1335228914177.jpg'),
-    new Recipe('A Test Recipe', "This is a test", 'http://foodjaunts.com/wp-content/uploads/2012/04/Spring-Shrimp-Salad-Overview-e1335228914177.jpg')
+    new Recipe('Another Test Recipe', "This is also a test", 'http://foodjaunts.com/wp-content/uploads/2012/04/Spring-Shrimp-Salad-Overview-e1335228914177.jpg')
   ];
 
   constructor() { }
@@ -18,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
 }
